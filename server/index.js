2abfd082ad40bc77a4ff3abd0f50fe5069f37a1d
ashddftw <ashudeepdubey8108@gmail.com>
2025-08-21 13:57:17 +0530

    testing

--- server/index.js
+++ server/index.js
@@ -38,5 +38,9 @@ app.use(async (req, res, next) => {
   next();
 });
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 module.exports = app;

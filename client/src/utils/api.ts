329d1198130b8ea708191daef309713a59b7c947
ashddftw <ashudeepdubey8108@gmail.com>
2025-08-21 13:21:44 +0530

    small changes

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: import.meta.env.PROD 
-    ? `${import.meta.env.VITE_API_URL || 'https://invoice-generator-4s59.vercel.app//'}/api`
+    ? `${import.meta.env.VITE_API_URL || 'https://invoice-generator-4s59.vercel.app'}/api`
     : '/api',
 });
 
